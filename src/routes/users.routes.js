8f4c7ae21930d880f2dfb1c6aa54d1366a79d4e6
ronaldo <ronaldo.dsantos@gmail.com>
2024-06-17 18:00:45 -0300

    Final version

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,12 @@
-const { Router } = require("express") // Importando o Router de dentro do próprio express
+const { Router } = require("express") 
 
-const UsersControllers = require("../controllers/UsersController")// Importando o arquivo UsersController
+const UsersControllers = require("../controllers/UsersController")
 
-const usersRoutes = Router() // Inicializando o Router do express
+const usersRoutes = Router() 
 
-const usersControllers = new UsersControllers() // Instanciando o UsersControllers, como ele é uma classe, precisamos alocar ele na memória para que póssamos utilizá-la
+const usersControllers = new UsersControllers() 
 
-usersRoutes.post("/", usersControllers.create) // Ao ser acessada a rota de usuários, está direcionando para a classe usersController e acessando o método create
-usersRoutes.put("/:id", usersControllers.update) // Rota de usuários acessada pelo método put, pegando um id por parametro e acessando o método update
+usersRoutes.post("/", usersControllers.create) 
+usersRoutes.put("/:id", usersControllers.update) 
 
-module.exports = usersRoutes // Exportando o usersRoutes para que o server.js possa utilizá-lo
+module.exports = usersRoutes 
